d33618b82859747505d7616f50905a320467c04d
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-11-17 23:27:26 +0200

    generic3

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,17 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
+type TopAndBottom = Pick<AllType, "name" | "position" | "color" | "weight">;
 
-function compare (top, bottom): AllType {
+function compare(top: TopAndBottom, bottom: TopAndBottom): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
